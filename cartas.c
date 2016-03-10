851c4841008f40f9f27d0f46454e313a12145387
jcm300 <jcm300@live.com.pt>
2016-03-10 11:01:02 +0000

    atualizado cartas

--- cartas.c
+++ cartas.c
@@ -8,6 +8,7 @@
 
 #define NAIPES		"DCHS"
 #define VALORES		"3456789TJQKA2"
+#define FORMATO "%lld_%lld_%lld_%lld_%d_%d_%d_%d_%lld_%d_%d_%d"
 
 /**
 Guarda o estado do jogo
@@ -19,10 +20,10 @@ Guarda o estado do jogo
 
 
 typedef struct {
-  	long long unsigned int mao[4];
+  	long long int mao[4];
   	int tamanho[4];
-  	long long unsigned int selecao;
-  	char acao;
+  	long long int selecao;
+  	int passar, jogar, selecionar;
 	} STATE;
 
 /**
@@ -33,6 +34,18 @@ Devolve o índice da carta
 */
 int indice(int naipe, int valor) {
 	return naipe * 13 + valor;
+}
+
+STATE str2estado (char* str) {
+	STATE e;
+	sscanf(str, FORMATO, &e.mao[0], &e.mao[1], &e.mao[2], &e.mao[3], &e.tamanho[0], &e.tamanho[1], &e.tamanho[2],&e.tamanho[3], &e.selecao, &e.passar, &e.selecionar, &e.jogar);
+	return e;
+}
+
+char* estado2str (STATE e){
+	static char res[10240];
+	sprintf(res, FORMATO, e.mao[0], e.mao[1], e.mao[2], e.mao[3], e.tamanho[0],e.tamanho[1],e.tamanho[2],e.tamanho[3], e.selecao, e.passar, e.selecionar, e.jogar);
+	return res;
 }
 
 /**
@@ -72,7 +85,7 @@ int carta_existe(long long int ESTADO, int naipe, int valor) {
 }
 
 
-int nrosCartas(long long unsigned int MAO){
+int nrosCartas(long long int MAO){
 	int  a = 0;
 	while(MAO != 0){
 		if(MAO % 2 == 1){
@@ -88,7 +101,11 @@ int nrosCartas(long long unsigned int MAO){
 /**
 Funçao que distribui as cartas
 */
-void distribuir(long long int ESTADO,long long unsigned int mao []) {
+STATE distribuir(STATE e) {
+	/**
+	Estado com todas as 52 cartas do baralho
+	*/
+	long long int ESTADO = 0xfffffffffffff;
 	int x,y,n;
 	srandom(time(NULL));
 	n=0;
@@ -96,9 +113,10 @@ void distribuir(long long int ESTADO,long long unsigned int mao []) {
 		x = random() % 4;
 		y = random() % 13;
 		if (carta_existe(ESTADO,x,y)) {
-			mao[0] = add_carta(mao[0],x,y);
+			e.mao[0] = add_carta(e.mao[0],x,y);
 			ESTADO = rem_carta(ESTADO,x,y);
 			n ++;
+			e.tamanho[0] += 1;
 		}
 	}
 	n=0;
@@ -106,9 +124,10 @@ void distribuir(long long int ESTADO,long long unsigned int mao []) {
 		x = random() % 4;
 		y = random() % 13;
 		if (carta_existe(ESTADO,x,y)) {
-			mao[1] = add_carta(mao[1],x,y);
+			e.mao[1] = add_carta(e.mao[1],x,y);
 			ESTADO = rem_carta(ESTADO,x,y);
 			n++;
+			e.tamanho[1] += 1;
 		}
 	}
 	n=0;
@@ -116,12 +135,15 @@ void distribuir(long long int ESTADO,long long unsigned int mao []) {
 		x = random() % 4;
 		y = random() % 13;
 		if (carta_existe(ESTADO,x,y)) {
-		mao[2] = add_carta(mao[2],x,y);
+		e.mao[2] = add_carta(e.mao[2],x,y);
 		ESTADO = rem_carta(ESTADO,x,y);
 		n ++;
+		e.tamanho[2] += 1;
 		}
 	}
-	mao[3] = ESTADO;
+	e.mao[3] = ESTADO;
+	e.tamanho[3] = 13;
+	return e;
 }
 
 /**
@@ -133,11 +155,12 @@ Imprime o html correspondente a uma carta
 @param naipe	O naipe da carta (inteiro entre 0 e 3)
 @param valor	O valor da carta (inteiro entre 0 e 12)
 */
-void imprime_carta(char *path, int x, int y, long long int ESTADO, int naipe, int valor) {
+void imprime_carta(char *path, int x, int y, STATE e, int i, int naipe, int valor) {
 	char *suit = NAIPES;
 	char *rank = VALORES;
-	char script[10240];
-	sprintf(script, "%s?q=%lld", SCRIPT, rem_carta(ESTADO, naipe, valor));
+	char script[10240]; 
+	rem_carta(e.mao[i], naipe, valor);
+	sprintf(script, "%s?%s", SCRIPT, estado2str(e));
 	printf("<a xlink:href = \"%s\"><image x = \"%d\" y = \"%d\" height = \"110\" width = \"80\" xlink:href = \"%s/%c%c.svg\" /></a>\n", script, x, y, path, rank[valor], suit[naipe]);
 }
 
@@ -155,7 +178,7 @@ Esta função está a imprimir o estado em quatro colunas: uma para cada naipe
 @param path	o URL correspondente à pasta que contém todas as cartas
 @param ESTADO	O estado atual
 */
-void imprime(char *path, long long unsigned int mao[]) {
+void imprime(char *path, STATE e) {
 	int n, v;
 	int x = 10;
 	int i, y;
@@ -168,9 +191,9 @@ void imprime(char *path, long long unsigned int mao[]) {
     for(i = 0, y = 10; i < 4; i ++, y += 120)
         for(x = 0, v = 0; v < 13; v++) {
                 for(n = 0; n < 4; n++)
-                    if(carta_existe(mao[i], n, v)) {
+                    if(carta_existe(e.mao[i], n, v)) {
                         x += 50;
-                        imprime_carta(path, x, y, mao[i], n, v);
+                        imprime_carta(path, x, y, e, i, n, v);
                     }
         }	
     
@@ -193,20 +216,11 @@ Cada carta corresponde a um bit que está a 1 se essa carta está no conjunto e 
 Caso não seja passado nada à cgi-bin, ela assume que todas as cartas estão presentes.
 @query A query que é passada à cgi-bin
  */
-void parse(char *query,STATE e) {
-	/**
-	Estado inicial com todas as 52 cartas do baralho
-	Cada carta é representada por um bit que está
-	a 1 caso ela pertença à mão ou 0 caso contrário
-	*/
-	const long long int ESTADO_INICIAL = 0xfffffffffffff;
-
-	if(sscanf(query, "q=%llu", e.mao) == 1) {
-		imprime(BARALHO, e.mao);
-	} else {
-		distribuir(ESTADO_INICIAL,e.mao);
-		imprime(BARALHO, e.mao);
+void parse(STATE e) {
+	if(e.mao[0] == 0) {
+		e = distribuir(e);
 	}
+	imprime(BARALHO, e);
 }
 
 /**
@@ -216,7 +230,13 @@ a função que vai imprimir o código html para desenhar as cartas
 int main() {
 
 	STATE e;
-	e.mao[0]=e.mao[1]=e.mao[2]=e.mao[3]=0;
+	int i;
+    for (i=0;i<4;i++) {
+    	e.mao[i]=0;
+    	e.tamanho[i]=0;
+    }
+    e.selecao=0;
+    e.passar = e.jogar = e.selecionar = 0;
 /*
  * Cabeçalhos necessários numa CGI
  */
@@ -225,13 +245,16 @@ int main() {
 	printf("<body>\n");
 
 	printf("<h1>Exemplo de utilização</h1>\n");
-    printf("<form action=\"http://127.0.0.1/cgi-bin/cartas\"><input type=\"submit\" value=\"Play\"></form>\n");
+    /*printf("<form action=\"http://127.0.0.1/cgi-bin/cartas\"><input type=\"submit\" value=\"Play\"></form>\n");
     printf("<form action=\"http://www.pagat.com/climbing/bigtwo.html\"><input type=\"submit\" value=\"Rules\"></form>\n");
-
+	*/
+    if (strlen(getenv("QUERY_STRING")) != 0){
+    	e = str2estado(getenv("QUERY_STRING"));
+    }
 /*
  * Ler os valores passados à cgi que estão na variável ambiente e passá-los ao programa
  */
-	parse(getenv("QUERY_STRING"), e);
+	parse(e);
 
 	printf("</body>\n");
 	return 0;
